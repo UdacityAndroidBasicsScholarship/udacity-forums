a9ddbef66cb06fe8ff507b64629f000afe856167
Gaurav kumar <gk07071999@gmail.com>
2018-04-18 17:29:45 +0530

    modified splashscreen

--- app/src/main/java/com/example/android/udacityforum/splash_screen.java
+++ app/src/main/java/com/example/android/udacityforum/splash_screen.java
@@ -1,9 +1,12 @@
 package com.example.android.udacityforum;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class splash_screen extends AppCompatActivity {
 
@@ -13,6 +16,13 @@ public class splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.notificationBar));
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
