17adc32b42a5a6eb08fac8b44ed4aa12b7b08d54
Gaurav kumar <gk07071999@gmail.com>
2018-04-29 18:27:05 +0530

    added login screen with firebase

--- app/src/main/java/com/example/android/udacityforum/activity_userpanel.java
+++ app/src/main/java/com/example/android/udacityforum/activity_userpanel.java
@@ -1,8 +1,11 @@
 package com.example.android.udacityforum;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
@@ -21,10 +24,25 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
+
+import java.io.IOException;
 
 public class activity_userpanel extends AppCompatActivity {
 
+    private int PICK_IMAGE_REQUEST = 1;
+    private ImageButton fab11;
+    private ImageView pic;
+    private TextView userNameLayout;
+    private TextView emailLayout;
+    private TextView UserEmail;
+    private TextView UserName;
+    private ImageView UserPic;
     NavigationView navigationView3;
     DrawerLayout drawerLayout3;
     Toolbar toolbar3;
@@ -53,6 +71,31 @@ public class activity_userpanel extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userpanel);
 
+        //For selecting picture from device when clicked on the imageButton.
+        fab11 = (ImageButton)findViewById(R.id.fab11);
+        fab11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, PICK_IMAGE_REQUEST);
+            }
+        });
+
+
+        pic = (ImageView)findViewById(R.id.imageButton);
+        userNameLayout = (TextView)findViewById(R.id.textView);
+        emailLayout = (TextView)findViewById(R.id.textView2);
+
+        final String name2 = getIntent().getExtras().getString("name2");
+        final String picURL2 = getIntent().getExtras().getString("picURL2");
+        final String Email2 = getIntent().getExtras().getString("Email2");
+
+        //setting the imported data to the views.
+        userNameLayout.setText(name2);
+        emailLayout.setText(Email2);
+        Glide.with(activity_userpanel.this).load(picURL2).into(pic);
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -72,19 +115,37 @@ public class activity_userpanel extends AppCompatActivity {
         navigationView3 = findViewById(R.id.navigation_drawer);
         drawerLayout3 = (DrawerLayout) findViewById(R.id.nav_drawer3);
         actionBarDrawerToggle3 = new ActionBarDrawerToggle(this, drawerLayout3, toolbar3, R.string.open_drawer, R.string.close_drawer);
-
         drawerLayout3.addDrawerListener(actionBarDrawerToggle3);
+        //setting the user data to fields.
+        View header = navigationView3.getHeaderView(0);
+        UserName = (TextView) header.findViewById(R.id.tv_name);
+        UserName.setText(name2);
+        UserPic = (ImageView) header.findViewById(R.id.iv_profile);
+        Glide.with(activity_userpanel.this).load(picURL2).into(UserPic);
+        UserEmail = (TextView) header.findViewById(R.id.tv_email);
+        UserEmail.setText(Email2);
+
         navigationView3.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
                 switch (id) {
                     case R.id.menu_profile:
-                        startActivity(new Intent(activity_userpanel.this, activity_userpanel.class));
+                        Intent o = new Intent(activity_userpanel.this, activity_userpanel.class);
+                        startActivity(o);
+                        o.putExtra("name2", name2);
+                        o.putExtra("picURL2", picURL2);
+                        o.putExtra("Email2", Email2);
+                        startActivity(o);
                         break;
 
                     case R.id.menu_home:
                         startActivity(new Intent(activity_userpanel.this, MainActivity.class));
+                        break;
+
+                    case R.id.menu_logout:
+                        AuthUI.getInstance().signOut(activity_userpanel.this);
+                        AppExit();
                         break;
                 }
                 return false;
@@ -172,8 +233,43 @@ public class activity_userpanel extends AppCompatActivity {
             // Show 4 total pages.
             return 4;
         }
+    }
+
+    //getting the image.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                ImageView imageView = (ImageView) findViewById(R.id.imageButton);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void AppExit()
+    {
+
+        this.finish();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    /*int pid = android.os.Process.myPid();=====> use this if you want to kill your activity. But its not a good one to do.
+    android.os.Process.killProcess(pid);*/
 
     }
+
 
 
 }
