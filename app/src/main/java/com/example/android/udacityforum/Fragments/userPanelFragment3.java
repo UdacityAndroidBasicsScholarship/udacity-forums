0e6b0b2862740b8a8b994c8b22a9a8e63812d507
Gaurav kumar <gk07071999@gmail.com>
2018-05-02 02:14:34 +0530

    sync

--- app/src/main/java/com/example/android/udacityforum/Fragments/userPanelFragment3.java
+++ app/src/main/java/com/example/android/udacityforum/Fragments/userPanelFragment3.java
@@ -1,18 +1,19 @@
-package com.example.android.udacityforum;
+package com.example.android.udacityforum.Fragments;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.udacityforum.R;
 
-public class userPanelFragment1 extends Fragment{
+public class userPanelFragment3 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_summary_userpanel, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_notifications_userpanel, container, false);
         return rootView;
     }
 }
--- app/src/main/java/com/example/android/udacityforum/NewQuestionActivity.java
+++ app/src/main/java/com/example/android/udacityforum/NewQuestionActivity.java
@@ -1,22 +1,24 @@
 package com.example.android.udacityforum;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
 public class NewQuestionActivity extends AppCompatActivity {
     EditText quesTitle;
     EditText quesBody;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_question);
-        quesTitle=findViewById(R.id.title);
-        quesBody=findViewById(R.id.body);
+        quesTitle = findViewById(R.id.title);
+        quesBody = findViewById(R.id.body);
     }
-    public void submitQuestion(View v){
+
+    public void submitQuestion(View v) {
         Intent intent = new Intent(this, ForumActivity.class);
         startActivity(intent);
     }
