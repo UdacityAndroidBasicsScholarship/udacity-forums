83b91a0e74939361522abc223bde169f268fb083
sourabhgupta811 <32221826+sourabhgupta811@users.noreply.github.com>
2018-05-01 21:06:59 +0530

    removed unnecessary code and implemented some navigation flow

--- app/src/main/java/com/example/android/udacityforum/NewQuestionActivity.java
+++ app/src/main/java/com/example/android/udacityforum/NewQuestionActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.udacityforum;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -19,26 +20,11 @@ public class NewQuestionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_question);
         quesTitle=findViewById(R.id.title);
         quesBody=findViewById(R.id.body);
-        db=openOrCreateDatabase("ForumDB",MODE_PRIVATE,null);
-        db.execSQL("create table if not exists questionsTable(title varchar,body varchar)");
     }
     public void submitQuestion(View v){
         String title=quesTitle.getText().toString();
         String body=quesBody.getText().toString();
-        ContentValues values=new ContentValues();
-        values.put("title",title);
-        values.put("body",body);
-        try {
-            db.insert("questionsTable", null, values);
-        }catch(Exception e){
-            Log.d("critical error","error occured in quesTable insert");
-        }
-//        Cursor cursor=db.rawQuery("select * from questionsTable",null);
-//        while(cursor.moveToNext()){
-//            String tabledatatitle=cursor.getString(0);
-//            String tabledatabody=cursor.getString(1);
-//            Log.d(tabledatatitle,tabledatabody);
-//        }
-
+        Intent intent = new Intent(this, ForumActivity.class);
+        startActivity(intent);
     }
 }
