955c41114c50e7f30d0fb2cd34fad74add723a51
sourabhgupta811 <32221826+sourabhgupta811@users.noreply.github.com>
2018-05-01 21:10:19 +0530

    removed unnecessary imports

--- app/src/main/java/com/example/android/udacityforum/NewQuestionActivity.java
+++ app/src/main/java/com/example/android/udacityforum/NewQuestionActivity.java
@@ -1,19 +1,14 @@
 package com.example.android.udacityforum;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class NewQuestionActivity extends AppCompatActivity {
     EditText quesTitle;
     EditText quesBody;
-    SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +17,6 @@ public class NewQuestionActivity extends AppCompatActivity {
         quesBody=findViewById(R.id.body);
     }
     public void submitQuestion(View v){
-        String title=quesTitle.getText().toString();
-        String body=quesBody.getText().toString();
         Intent intent = new Intent(this, ForumActivity.class);
         startActivity(intent);
     }
