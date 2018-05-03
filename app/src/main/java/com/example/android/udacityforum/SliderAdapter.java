cc2d3702079c61b5ae58fb63e579deef4c19421d
sourabhgupta811 <32221826+sourabhgupta811@users.noreply.github.com>
2018-05-03 11:07:32 +0530

    rearranged and reformatted code and changed variables signatures and modifiers

--- app/src/main/java/com/example/android/udacityforum/SliderAdapter.java
+++ app/src/main/java/com/example/android/udacityforum/SliderAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.udacityforum;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,52 +15,52 @@ import android.widget.TextView;
  */
 
 public class SliderAdapter extends PagerAdapter {
-    Context context;
-    LayoutInflater layoutInflater;
-
-    public SliderAdapter(Context context){
-        this.context=context;
-    }
-
-    // Arrays
-
-    public int[] Slide_images={
-            R.drawable.app_icon,
-            R.drawable.ask,
-            R.drawable.get,
-            R.drawable.comments
-    };
-    public String[] slide_headings={
-            "Welcome to Udacity Forums ",
-            "Ask Questions !",
-            "Get Answers !",
-            "Comments on your question."
-    };
-    public String[] description={
+    public String[] description = {
 
             "Ask any questions and get your answers.",
             "Ask questions or query in these app.",
             "Get solved answers from our community.",
             "In the comment section you will be able to see comments on his/her question and can reply back for more discussion.."
     };
+    private int[] Slide_images = {
+            R.drawable.app_icon,
+            R.drawable.ask,
+            R.drawable.get,
+            R.drawable.comments
+    };
+    private String[] slide_headings = {
+            "Welcome to Udacity Forums ",
+            "Ask Questions !",
+            "Get Answers !",
+            "Comments on your question."
+    };
+    // Arrays
+    private Context context;
+    private LayoutInflater layoutInflater;
+
+    SliderAdapter(Context context) {
+        this.context = context;
+    }
+
     @Override
     public int getCount() {
         return slide_headings.length;
     }
 
     @Override
-    public boolean isViewFromObject(View view, Object o){
-        return view == (RelativeLayout)o;
+    public boolean isViewFromObject(View view, Object o) {
+        return view == (RelativeLayout) o;
     }
 
+    @NonNull
     @Override
-    public Object instantiateItem(ViewGroup container, int position){
-        layoutInflater=(LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-        View view=layoutInflater.inflate(R.layout.slide_layout,container,false);
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
 
-        ImageView slideImageView=(ImageView) view.findViewById(R.id.slideimage);
-        TextView slideheading=(TextView) view.findViewById(R.id.heading);
-        TextView slidedescription=(TextView) view.findViewById(R.id.description);
+        ImageView slideImageView = (ImageView) view.findViewById(R.id.slideimage);
+        TextView slideheading = (TextView) view.findViewById(R.id.heading);
+        TextView slidedescription = (TextView) view.findViewById(R.id.description);
 
         slideImageView.setImageResource(Slide_images[position]);
         slideheading.setText(slide_headings[position]);
@@ -73,8 +74,8 @@ public class SliderAdapter extends PagerAdapter {
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object){
-        container.removeView((RelativeLayout)object);
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((RelativeLayout) object);
     }
 
 
