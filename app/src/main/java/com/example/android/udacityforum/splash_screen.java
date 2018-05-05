cc0c9cbb710743ae6d754a77838cd1e4569f1a56
vipul Aggarwal <vipul.captureart@gmail.com>
2018-05-06 01:02:03 +0530

    Pull Request For Splash Screen

--- app/src/main/java/com/example/android/udacityforum/splash_screen.java
+++ app/src/main/java/com/example/android/udacityforum/splash_screen.java
@@ -7,37 +7,41 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.annotation.Annotation;
+
+import static java.lang.Thread.sleep;
 
 public class splash_screen extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 3000;
-    Handler handler;
+    private TextView tv;
+    private ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-        if (Build.VERSION.SDK_INT >= 21) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(getResources().getColor(R.color.colorBlack));
-        }
-
-        handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent j = new Intent(splash_screen.this, ActivitySlider.class);
-                startActivity(j);
-                finish();
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Annotation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(this, MainActivity.class);
+        Thread timer = new Thread{
+                public void run() {
+                    try {
+                        sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    finally {
+                        startActivity(i);
+                        finish();
+                    }
             }
-        }, SPLASH_TIME_OUT);
-    }
-
-    @Override
-    protected void onDestroy() {
-        handler.removeCallbacksAndMessages(null);
-        super.onDestroy();
+    };
+                timer.start();
     }
 }
