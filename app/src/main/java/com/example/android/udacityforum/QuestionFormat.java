7e19262d4667b21d58e05f6f93bf10424b13bd6e
sourabhgupta811 <sourabh@totalitycorp.com>
2018-10-19 23:20:06 +0530

    fixed variables in forum activity

--- app/src/main/java/com/example/android/udacityforum/QuestionFormat.java
+++ app/src/main/java/com/example/android/udacityforum/QuestionFormat.java
@@ -3,10 +3,35 @@ package com.example.android.udacityforum;
 class QuestionFormat {
     private String quesHeading;
     private String quesContent;
+    private long questionID;
+
+
+    public QuestionFormat(String quesHeading, String quesContent, long questionID) {
+        this.quesHeading = quesHeading;
+        this.quesContent = quesContent;
+        this.questionID = questionID;
+    }
+
+    public void setQuesHeading(String quesHeading) {
+        this.quesHeading = quesHeading;
+    }
+
+    public void setQuesContent(String quesContent) {
+        this.quesContent = quesContent;
+    }
+
+    public long getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(long questionID) {
+        this.questionID = questionID;
+    }
 
     public QuestionFormat(String heading, String content) {
         this.quesContent = content;
         this.quesHeading = heading;
+
     }
 
     public String getQuesHeading() {
